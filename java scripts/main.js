04b5a2b53c5b18f74e78c803cbfedcb3d5dee7ce
YazanHaitham2000 <khaitham1999@gmail.com>
2024-06-02 21:36:21 +0300

    Add files via upload

---
+++ java scripts/main.js
@@ -0,0 +1,29 @@
+/**------1------*/var Name="john"; console.log(Name);
+
+/**------2------*/const PI=3.14;console.log(PI);
+
+/**------3------*/var age=24; console.log(age);
+
+/**------4------*/var isStudent=true; console.log(isStudent);
+
+/**------5------*/let message= "Hello my Name is yazan"; console.log(message);
+
+/**------6------*/var Name="yazan"; var age=24; console.log(Name,age);
+
+/**------7------*/var message= "Hello my Name is yazan"; console.log(message.length);
+
+/**------8------*/var message= "hello my name is yazan"; var upper=message.toUpperCase(); console.log(upper);
+
+/**------9------*/var message= "HELLO MY NAME IS YAZAN"; var lower=message.toLowerCase(); console.log(lower);
+
+/**------10------*/var ind="I love learning JavaScript"; var inde=ind.indexOf("JavaScript") ;console.log(inde);
+
+/**------11------*/var message= "Hello my Name is yazan"; var msg=message.slice(3,20); console.log(msg);
+
+/**------12------*/let num1=10;
+
+/**------13------*/let num2=20;
+
+/**------14------*/let num1=10; let num2=20; let sum=num1+num2; console.log(sum);
+
+/**------15------*/let num1=10; let num2=20; let subtrac=num1-num2; console.log(subtrac);
